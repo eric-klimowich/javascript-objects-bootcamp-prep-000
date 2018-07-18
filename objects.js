90364931610e2e29e9d7670959e796081a95784b
Eric Klimowich <eklimowich@hotmail.com>
2018-07-18 07:27:19 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { MichaelJackson: "Beat It" };
+
+function updatePlaylist(playlist, artistName, songTitle)
+{
+  playlist.artistName = songTitle
+  return playlist 
+}
+
+function removeFromPlaylist(playlist, artistName)
+{
+  
+}
